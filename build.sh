a02e2413ba43ba63704bfa6a07d51a985a716e8d
Sergio Valverde <sergio.valverde@imdea.org>
2018-01-15 12:11:10 +0100

    Script to run before running dynodroid for the first time

---
+++ build.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ] || [ $# -gt 2 ]; then
+  echo "Usage: ${0} <ANDROID_HOME> [TOOLDIRNAME]"
+  exit 1
+fi
+
+ANDROID_HOME=$1
+if [[ ! -e ${ANDROID_HOME} ]]; then
+    echo "This ANDROID_HOME: ${ANDROID_HOME} - Doesn't exist."
+    echo "Run again with a correct folder."
+    exit 1
+fi
+
+TOOLDIRNAME=${2:-deploy}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+TOOLDIR=${DIR}/${TOOLDIRNAME}/
+
+
+echo "-- PREPARING ${TOOL}"
+echo -n "    * Downloading sdcard.img"
+# Thanks to https://stackoverflow.com/a/38937732/8091456
+ggID='1mf4kKCNgz059C5hzRKOT5Wk_e9WccmFk'
+ggURL='https://drive.google.com/uc?export=download'
+filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
+echo ${filename}
+getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
+URL="${ggURL}&confirm=${getcode}&id=${ggID}"
+curl -Lb /tmp/gcokie ${URL} -o ${filename}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1 || echo "OK"
+
+echo -n "    * "
+CMD="mv ${filename} ${DIR}/dynodroidsetup/freshavd/emu.avd/"
+echo -n "${CMD}: "
+eval ${CMD}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1 || echo "OK"
+
+
+echo "-- BUILDING ${TOOL}"
+echo -n "    * "
+CMD="export SDK_INSTALL=${ANDROID_HOME}"
+echo -n "${CMD}: "
+eval ${CMD}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1 || echo "OK"
+
+cd ${DIR}
+echo -n "    * cleaning ${TOOLDIR}"
+rm -rf ${TOOLDIR}
+mkdir -p ${TOOLDIR}
+
+echo -n "    * building... "
+CMD="python dynodroidsetup.py ${DIR} ${TOOLDIR}"
+echo -n "${CMD}: "
+eval ${CMD}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1 || echo "OK"
+
+cd ${TOOLDIR}
+
+echo -n "    * "
+CMD="ant -diagnostics"
+echo -n "${CMD}: "
+eval ${CMD}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1 || echo "OK"
+
+echo -n "    * "
+CMD="ant -verbose -debug clean"
+echo -n "${CMD}: "
+eval ${CMD}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1 || echo "OK"
+
+
+echo -n "    * "
+CMD="ant -verbose -debug compile"
+echo -n "${CMD}: "
+eval ${CMD}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1 || echo "OK"
