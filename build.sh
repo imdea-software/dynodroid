4b40d8001b6c58afba29cc0e394f155eb3f2ec77
svg153 <svg153@gmail.com>
2018-12-31 04:03:31 +0100

    paths improved

--- build.sh
+++ build.sh
@@ -20,29 +20,35 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 TOOLDIR=${DIR}/${TOOLDIRNAME}/
-
+setupRelPath="/dynodroidsetup"
+# setupRelPath same as dynodroid/dynodroidsetup.py
+sdcardimg_setup_filepath="${DIR}${setupRelPath}/freshavd/emu.avd/sdcard.img"
+sdcardimg_backup_filepath="${DIR}/freshavd/emu.avd/sdcard.img"
 
 echo "-- PREPARING ${TOOL}"
-if [[ -f "${DIR}/fresavd/sdcard.img" ]]; then
+if [[ -f "${sdcardimg_backup_filepath}" ]]; then
   echo -n "    * Coping sdcard.img backup: "
-  filename="sdcard.img"
-  cp "${DIR}/fresavd/${filename}" "${DIR}/"
+  sdcardimg_filename="sdcard.img"
+  sdcardimg_filepath="${DIR}/${sdcardimg_filename}"
+  CMD="cp ${DIR}/freshavd/emu.avd/sdcard.img ${sdcardimg_filepath}"
+  echo -n "${CMD}: "
   [[ $? -ne 0 ]] && echo "ERROR" && exit 1 || echo "OK"
 else
   echo "    * Downloading sdcard.img"
   # Thanks to https://stackoverflow.com/a/38937732/8091456
   ggID='1mf4kKCNgz059C5hzRKOT5Wk_e9WccmFk'
   ggURL='https://drive.google.com/uc?export=download'
-  filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
+  sdcardimg_filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
+  sdcardimg_filepath="${DIR}/${sdcardimg_filename}"
   getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
   URL="${ggURL}&confirm=${getcode}&id=${ggID}"
-  curl -Lb /tmp/gcokie ${URL} -o ${filename}
+  curl -Lb /tmp/gcokie ${URL} -o ${sdcardimg_filepath}
   echo -n "    * Downloading sdcard.img: "
   [[ $? -ne 0 ]] && echo "ERROR" && exit 1 || echo "OK"
 fi
 
 echo -n "    * "
-CMD="mv ${filename} ${DIR}/dynodroidsetup/freshavd/emu.avd/"
+CMD="mv ${sdcardimg_filepath} ${sdcardimg_setup_filepath}"
 echo -n "${CMD}: "
 eval ${CMD}
 [[ $? -ne 0 ]] && echo "ERROR" && exit 1 || echo "OK"
